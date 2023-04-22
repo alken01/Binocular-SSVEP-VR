8f3a34ba7c7caa0498fe61ebcd52302aa744010d
Alken Rrokaj <rrokajalken@gmail.com>
2023-04-22 17:01:24 +0200

    22april

--- Assets/Stimulus/SSVEP.cs
+++ Assets/Stimulus/SSVEP.cs
@@ -25,7 +25,7 @@ public class SSVEP : MonoBehaviour {
 
         float value;
         if (sine) value = (Mathf.Sin(t * Mathf.PI * 2f) + 1f) / 2f; // sine wave
-        else value = Mathf.Repeat(t, 1f) < 0.5f ? 0f : 1f; // square wave
+        else value = Mathf.Repeat(t/2, 1f) < 0.5f ? 0f : 1f; // square wave
 
         // Change the alpha value
         objectRenderer.material.color = new Color(1f, 1f, 1f, value);
